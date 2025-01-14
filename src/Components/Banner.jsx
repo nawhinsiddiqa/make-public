c844042a0fd06785ebf71955f9286d20de76d123
nawshin <nawshinsiddiqa@gmail.com>
2025-01-14 17:51:07 +0600

    added banner section

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,29 @@
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const Banner = () => {
     return (
         <div>
+            <Carousel>
+                <div className="max-w-screen-xl mx-auto">
+                    <img className=""
+                     src="https://i.ibb.co.com/3MrB113/flat-design-pet-sitting-facebook-cover-23-2149641135.jpg"></img>
+                  
+                </div>
+                <div>
+                    <img src="https://i.ibb.co.com/TTzGjwC/Gear-New-Pet-1168772154.webp"></img>
+               
+                </div>
+                <div>
+                    <img src="https://i.ibb.co.com/B3Q4xJT/adopt-pet-template-banner-23-2148555612-1.jpg"></img>
+                    
+                </div>
+                <div>
+                    <img src="https://i.ibb.co.com/94VNx5Q/stock-photo-funny-gray-kitten-and-smiling-dog-with-beautiful-big-eyes-on-trendy-blue-background-love.jpg"></img>
             
+                </div>
+            </Carousel>
+
         </div>
     );
 };
